a845fa08aceb02169748508db827700131f5092e
XUANXUQAQ <xuanxusb@outlook.com>
2023-04-05 10:54:44 +0800

    自动添加缺失的json字段

--- src/main/java/file/engine/nouac/Plugin/configs/ProgramConfigs.java
+++ src/main/java/file/engine/nouac/Plugin/configs/ProgramConfigs.java
@@ -1,9 +1,9 @@
 package file.engine.nouac.Plugin.configs;
 
 
+import com.google.gson.Gson;
 import file.engine.nouac.Plugin.utils.OpenUtils;
 import file.engine.nouac.Plugin.utils.gson.GsonUtil;
-import com.google.gson.Gson;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
@@ -54,25 +54,34 @@ public enum ProgramConfigs {
             Files.createFile(_savePath);
         }
         Gson gson = GsonUtil.INSTANCE.getGson();
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(savePath), StandardCharsets.UTF_8))) {
+        var sb = new StringBuilder();
+        try (var reader = new BufferedReader(new InputStreamReader(new FileInputStream(savePath), StandardCharsets.UTF_8))) {
             String line;
-            StringBuilder sb = new StringBuilder();
             while ((line = reader.readLine()) != null) {
                 sb.append(line);
             }
-            List list = gson.fromJson(sb.toString(), List.class);
-            if (list != null) {
-                programs.clear();
-                list.forEach(each -> {
-                    Map<String, String> tmp = (Map<String, String>) each;
-                    String path = tmp.get("path");
-                    String workingDir = tmp.get("workingDir");
-                    String params = tmp.get("params");
-                    String checkExeName = tmp.get("checkExeName");
-                    LaunchWrapper launchWrapper = new LaunchWrapper(path, workingDir, params, checkExeName);
-                    programs.add(launchWrapper);
-                });
-            }
+        }
+        List list = gson.fromJson(sb.toString(), List.class);
+        if (list != null) {
+            programs.clear();
+            list.forEach(each -> {
+                Map<String, String> tmp = (Map<String, String>) each;
+                String path = tmp.getOrDefault("path", "");
+                String workingDir = tmp.getOrDefault("workingDir", "");
+                String params = tmp.getOrDefault("params", "");
+                String checkExeName = tmp.getOrDefault("checkExeName", "");
+                var launchWrapper = new LaunchWrapper(path, workingDir, params, checkExeName);
+                programs.add(launchWrapper);
+            });
+            writeToFile();
+        }
+    }
+
+    private void writeToFile() throws IOException {
+        Gson gson = GsonUtil.INSTANCE.getGson();
+        String json = gson.toJson(programs);
+        try (var writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(savePath), StandardCharsets.UTF_8))) {
+            writer.write(json);
         }
     }
 }
--- src/main/java/file/engine/nouac/Plugin/utils/VersionUtil.java
+++ src/main/java/file/engine/nouac/Plugin/utils/VersionUtil.java
@@ -13,7 +13,7 @@ import java.util.Map;
 
 public class VersionUtil {
 
-    private final static double CURRENT_VERSION = 1.1;
+    private final static double CURRENT_VERSION = 1.2;
 
     private static String updateURL;
 
